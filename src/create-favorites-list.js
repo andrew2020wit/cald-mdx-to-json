cc424a67de10bac22a1f2805e2b3c3dab96899bf
Andrew Faurash <andrew2020wit@gmail.com>
2024-03-13 08:34:29 +0200

    create-favorites-list.js mixIt

--- src/create-favorites-list.js
+++ src/create-favorites-list.js
@@ -1,6 +1,6 @@
 import {writeFileSync, readFileSync} from 'node:fs';
 
-const resultExamples = [];
+let resultExamples = [];
 const dictionaryTag = 'phv-A1'
 const outputPath = './output/cald-examples-' + dictionaryTag + '.json';
 const dictionaryPath = './output/cald-dictionary.json';
@@ -12,6 +12,8 @@ function definitionFilter(def) {
 function partOfSpeechFilter(article) {
     return article.partOfSpeech === 'phrasal verb';
 }
+
+const mixIt = true;
 
 // Example result item:
 // {
@@ -31,6 +33,34 @@ function partOfSpeechFilter(article) {
 const dictionary = JSON.parse(readFileSync(dictionaryPath, 'utf-8'));
 
 dictionary.terms.forEach(term => computeTerm(term));
+
+if (mixIt) {
+    const length1 = resultExamples.length;
+
+    const oldArr = [...resultExamples];
+    resultExamples = [];
+
+    while (oldArr.length) {
+        const index = Math.round(Math.random() * (oldArr.length -1));
+        const item = oldArr.splice(index, 1)[0];
+        if (!item) {
+            throw '!item';
+        }
+        resultExamples.push(item);
+    }
+
+    const length2 = resultExamples.length;
+
+    if (length1 !== length2) {
+        throw 'length1 !== length2';
+    }
+}
+
+console.log('Examples: ', resultExamples.length);
+
+writeFileSync(outputPath, JSON.stringify({favoriteExamples: resultExamples}, null, 2));
+
+console.log('======== Done! ============');
 
 function computeTerm(term) {
     term.articles.forEach(article => computeArticle(article, term));
@@ -56,7 +86,3 @@ function computeDef(term, def, partOfSpeech, transcription) {
         })
     }
 }
-
-writeFileSync(outputPath, JSON.stringify({favoriteExamples: resultExamples}, null, 2));
-
-console.log('======== Done! ============');
